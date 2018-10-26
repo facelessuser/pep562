b1fcf20c6ebeb420946ae1c516a1206c5b0434eb
facelessuser <faceless.shop@gmail.com>
2018-10-26 08:34:12 -0600

    Provide better version info object

--- pep562/__meta__.py
+++ pep562/__meta__.py
@@ -1,70 +1,117 @@
-"""Meta Data."""
+"""
+Meta related things.
+
+Get the version (PEP 440).
+
+Version structure which is sorted for comparisons `v1 > v2` etc.
+  (major, minor, micro, release type, pre-release build, post-release build)
+Release types are named is such a way they are comparable with ease.
+
+- pre-releases will have a pre-release build number greater than 0
+- post-release is only applied if post-release build is greater than 0
+- development branches can be applied to pre, post, or final builds as development.
+  (`DEV` being equvialent to `DEV_FINAL`). All `DEV` releases are sorted before even `ALPHA`.
+- `DEV` is currently always appended to the end of the release string and given the build of 0.
+  As we don't manage build releases, `DEV` is more of a way to mark that the current code is in development.
+  If we were doing regular builds and putting them on a server, then the build number would be more important.
+
+Acceptable version releases:
+
+```
+(1, 0, 0, FINAL, 0, 0)     1.0
+(1, 2, 0, FINAL, 0, 0)     1.2
+(1, 2, 3, FINAL, 0, 0)     1.2.3
+(1, 2, 0, ALPHA, 4, 0)     1.2a4
+(1, 2, 0, BETA, 4, 0)      1.2b4
+(1, 2, 0, RC, 4, 0)        1.2rc4
+(1, 2, 0, BETA, 1, 1)      1.2b1.post1
+(1, 2, 0, FINAL, 0, 1)     1.2.post1
+(1, 2, 3, DEV_ALPHA, 1, 0) 1.2.3a1.dev0
+(1, 2, 3, DEV, 0, 0)       1.2.3.dev0
+(1, 2, 3, DEV, 0, 1)       1.2.3.post1.dev0
+(1, 2, 3, DEV_BETA, 2, 1)  1.2.3b2.post1.dev0
+```
+
+"""
+from collections import namedtuple
+
+DEV_ALPHA = 0
+DEV_BETA = 1
+DEV_RC = 2
+DEV = 3
+ALPHA = 4
+BETA = 5
+RC = 6
+FINAL = 7
+
+PRE_REL = (DEV_ALPHA, DEV_BETA, DEV_RC, ALPHA, BETA, RC)
+DEV_REL = (DEV_ALPHA, DEV_BETA, DEV_RC, DEV)
+REL_MAP = {DEV_ALPHA: "a", DEV_BETA: "b", DEV_RC: "rc", DEV: "", ALPHA: "a", BETA: "b", RC: "rc", FINAL: ""}
+DEV_STATUS = {
+    DEV: '2 - Pre-Alpha',
+    DEV_ALPHA: '2 - Pre-Alpha',
+    DEV_BETA: '2 - Pre-Alpha',
+    DEV_RC: '2 - Pre-Alpha',
+    ALPHA: '3 - Alpha',
+    BETA: '4 - Beta',
+    RC: '4 - Beta',
+    FINAL: '5 - Production/Stable'
+}
+
+
+class Pep440Version(namedtuple('Pep440Version', ['epoch', 'major', 'minor', 'release', 'pre', 'post'])):
+    """Pep440 version."""
+
+    def __new__(cls, epoch, major, minor, release, pre, post):
+        """Validate version info."""
+
+        # Should be a valid release.
+        if not (release in REL_MAP):  # pragma: no cover
+            raise ValueError("The value '{}' does not indicate a valid release type.".format(vi.release))
+        # Pre-release releases should have a pre-release value.
+        if not (pre > 0 if release in PRE_REL else pre == 0):  # pragma: no cover
+            raise ValueError("Prereleases should have a value greater than '0'.")
+
+        return super(Pep440Version, cls).__new__(cls, epoch, major, minor, release, pre, post)
+
+    def _is_pre(self):
+        """Is prerelease."""
+
+        return self.release in PRE_REL
+
+    def _is_dev(self):
+        """Is development."""
+
+        return self.release in DEV_REL
+
+    def _is_post(self):
+        """Is post."""
+
+        return self.post > 0
+
+    def _get_dev_status(self):  # pragma: no cover
+        """Get development status string."""
+
+        return DEV_STATUS[self.release]
+
+    def _get_canonical(self):
+        """Get the canonical output string."""
+
+        # Assemble epoch, major, minor version and append `pre`, `post`, or `dev` if needed..
+        if self.minor == 0:
+            ver = '{}.{}'.format(self.epoch, self.major)
+        else:
+            ver = '{}.{}.{}'.format(self.epoch, self.major, self.minor)
+        if self._is_pre():
+            ver += '{}{}'.format(REL_MAP[self.release], self.pre)
+        if self._is_post():
+            ver += '.post{}'.format(self.post)
+        if self._is_dev():
+            ver += '.dev0'
+
+        return ver
+
 
 #   (major, minor, micro, release type, pre-release build, post-release build, development-release)
-__version_info__ = (1, 0, 0, 'beta', 1, 0)
-
-
-def pep440_version(vi):
-    """
-    Get the version (PEP 440).
-
-    Version structure
-      (major, minor, micro, release type, pre-release build, post-release build)
-    Release names are named is such a way they are sortable and comparable with ease.
-      (`.dev-alpha` | `.dev-beta` | `.dev-canidate` | `.dev-final` | `alpha` | `beta` | `candidate` | `final`)
-
-    - pre-releases will have a pre-release build number greater than 0
-    - post-release is only applied if post-release build is greater than 0
-    - development branches append `.dev-`. You can mark pre, post, or final builds as development.
-      All `.dev-` releases are sorted before even `alpha`, with `.dev-alpha` coming first, and `.dev-final` coming last.
-    - `.dev` is always appended to the end of the releases string and given the build of 0.
-      As we don't manage build releases, `.dev` is more of a way to mark that the current code is development.
-      If we were doing regular builds and putting them on a server, then the build number would be more important.
-
-    Acceptable version releases:
-
-    ```
-    (1, 0, 0, 'final', 0, 0)      1.0
-    (1, 2, 0, 'final', 0, 0)      1.2
-    (1, 2, 3, 'final', 0, 0)      1.2.3
-    (1, 2, 0, 'alpha', 4, 0)      1.2a4
-    (1, 2, 0, 'beta', 4, 0)       1.2b4
-    (1, 2, 0, 'candidate', 4, 0)  1.2rc4
-    (1, 2, 0, 'beta', 1, 1):      1.2b1.post1
-    (1, 2, 0, 'final', 0, 1)      1.2.post1
-    (1, 2, 3, '.dev-alpha', 1, 0) 1.2.3a1.dev0
-    (1, 2, 3, '.dev-final', 0, 0) 1.2.3.dev0
-    (1, 2, 3, '.dev-final', 0, 1) 1.2.3.post1.dev0
-    (1, 2, 3, '.dev-beta', 2, 1)  1.2.3b2.post1.dev0
-    ```
-
-    """
-
-    # Determine if this is a development build or regular build
-    dev, rel_type = (True, vi[3][5:]) if vi[3].startswith('.dev-') else (False, vi[3])
-
-    releases = {"alpha": 'a', "beta": 'b', "candidate": 'rc', "final": ''}
-
-    # Ensure version info is valid.
-    # Version info should be proper length.
-    assert len(vi) == 6
-    # Should be a valid release.
-    assert rel_type in releases
-    # Pre-release releases should have a pre-release value.
-    assert rel_type == 'final' or vi[4] > 0
-    # Final should not have a pre-release value.
-    assert rel_type != 'final' or vi[4] == 0
-
-    # Assemble major, minor, patch version. Patch can be left out if 0.
-    main = '.'.join(str(x) for x in (vi[0:2] if vi[2] == 0 else vi[0:3]))
-    # Assemble pre-release if needed.
-    prerel = releases[rel_type] + str(vi[4]) if releases[rel_type] else ''
-    # Assemble post-release if we have one.
-    postrel = '.post%d' % vi[5] if vi[5] > 0 else ''
-    # Assemble development release if we have one.
-    devrel = '.dev0' if dev else ''
-
-    return ''.join((main, prerel, postrel, devrel))
-
-
-__version__ = pep440_version(__version_info__)
+__version_info__ = Pep440Version(1, 0, 0, BETA, 1, 0)
+__version__ = __version_info__._get_canonical()
